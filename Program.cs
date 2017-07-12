c728082fd1e9fa78c697b291b8a56c3ad7f16668
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-07-12 11:25:17 -0500

    Did a bunch of linq stuff

---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You go this girl!");
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Longanberry"};
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit.StartsWith("L")
+                select fruit;
+
+            foreach (string thing in LFruits){
+                Console.WriteLine(thing);
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(num => num%4 == 0 | num%6 == 0);
+
+            foreach (int thing in fourSixMultiples){
+                Console.WriteLine(thing);
+            }
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+            {
+                "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                "Francisco", "Tre" 
+            };
+
+            IEnumerable<string> descend = names.OrderByDescending(thing => thing);
+
+            foreach (string thing in descend){
+                Console.WriteLine(thing);
+            }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var numbersAscend = numbers2.OrderBy(thing => thing);
+
+            foreach (int thing in numbersAscend){
+                Console.WriteLine(thing);
+            }
+
+            // Output how many numbers are in this list
+            List<int> numbers3 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var numList = numbers3.Count();
+
+            Console.WriteLine($"Numbers in numList: {numList}");
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+
+            Console.WriteLine($"We have made {purchases.Sum():C} money.");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+
+            Console.WriteLine($"Our most expensive product costs {prices.Max():C}.");
+
+            /*
+            Store each number in the following List until a perfect square
+            is detected.
+
+            Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<double> wheresSquaredo = new List<double>()
+            {
+            66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+            };
+
+            IEnumerable<double> squareList = wheresSquaredo.TakeWhile(thing => Math.Sqrt(thing) % 1 != 0);
+
+            foreach (double thing in squareList){
+                Console.WriteLine(thing);
+            }
+
+            // Create some banks and store in a List
+            List<Bank> banks = new List<Bank>() {
+                new Bank(){ Name="First Tennessee", Symbol="FTB"},
+                new Bank(){ Name="Wells Fargo", Symbol="WF"},
+                new Bank(){ Name="Bank of America", Symbol="BOA"},
+                new Bank(){ Name="Citibank", Symbol="CITI"},
+            };
+
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            /* 
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://code.msdn.microsoft.com/LINQ-to-DataSets-Grouping-c62703ea
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
+
+            IEnumerable<Customer> millionaireList = customers.Where(thing => thing.Balance >= 1000000);
+
+            foreach (var thing in millionaireList){
+                Console.WriteLine($"{thing.Name} {thing.Balance}");
+            }
+
+            var bankList = millionaireList.GroupBy(thing => thing.Bank, thing => thing.Name);
+
+            foreach (var thing in bankList){
+                Console.WriteLine(thing.Key + " " + thing.Count());
+            }
+        
+
+            // IEnumerable<Customer> milsWF = customers.Where(thing => thing.Bank.Contains("WF") && thing.Balance > 1000000);
+            // IEnumerable<Customer> milsBOA = customers.Where(thing => thing.Bank.Contains("BOA") && thing.Balance > 1000000);
+            // IEnumerable<Customer> milsFTB = customers.Where(thing => thing.Bank.Contains("FTB") && thing.Balance > 1000000);
+            // IEnumerable<Customer> milsCITI = customers.Where(thing => thing.Bank.Contains("CITI") && thing.Balance > 1000000);
+            // Console.WriteLine($"WF: {milsWF.Count()}");
+            // Console.WriteLine($"BOA: {milsBOA.Count()}");
+            // Console.WriteLine($"FTBWF: {milsFTB.Count()}");
+            // Console.WriteLine($"CITI: {milsCITI.Count()}");
+
+            /*
+                TASK:
+                As in the previous exercise, you're going to output the millionaires,
+                but you will also display the full name of the bank. You also need
+                to sort the millionaires' names, ascending by their LAST name.
+
+                Example output:
+                    Tina Fey at Citibank
+                    Joe Landy at Wells Fargo
+                    Sarah Ng at First Tennessee
+                    Les Paul at Wells Fargo
+                    Peg Vale at Bank of America
+            */
+
+            var newBankList = millionaireList.Join(banks,
+            customer => customer.Bank,
+            bank => bank.Symbol,
+            (customer, bank) => 
+                new { Customer = customer.Name, Bank = bank.Name});
+
+            foreach (var thing in newBankList) {
+                Console.WriteLine($"{thing.Customer} at {thing.Bank}");
+            }
+
+            
+
+        }
+    }
+
+    // Build a collection of customers who are millionaires
+    // Define a bank
+    public class Bank
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Define a customer
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+
+
+}
